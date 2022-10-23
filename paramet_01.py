445722f206664a9e75ca66955cb2f66e44d0c8ed
Luciano Daniel <42324730+lodaniel@users.noreply.github.com>
2022-10-23 08:19:27 -0300

    Add files via upload

---
+++ paramet_01.py
@@ -0,0 +1,11 @@
+# Curva parametrizada animada
+import numpy as np
+import turtle
+
+tela = turtle.Screen()
+for i in range(3000):
+    t = i / 80*np.pi
+    x = 200*np.cos(t)
+    y = 200*np.sin(2*t)
+    turtle.goto(x,y)
+screen.exitonclick()
